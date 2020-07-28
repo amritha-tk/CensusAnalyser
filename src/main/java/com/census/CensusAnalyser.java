be454d947ad34244728c1bb7c5f31fd62a0b0b19
amritha-tk <65520729+amritha-tk@users.noreply.github.com>
2020-07-28 16:18:33 +0530

    [amritha]Added:Census analyser to analyse and load Indian states census from a csv file and count the number of entries

---
+++ src/main/java/com/census/CensusAnalyser.java
@@ -0,0 +1,25 @@
+package com.census;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+public class CensusAnalyser {
+    public int loadIndiaCensusData(String csvFilePath) throws IOException {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            ;
+            Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            return numOfEntries;
+    }
+}
